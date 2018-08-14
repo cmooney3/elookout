1acf51f1b3bbeef3c5e31f136fb3680cd7037871
Charlie Mooney <cmooney3@gmail.com>
2018-08-14 16:05:41 -0700

    A few more fixed to the build script/etc
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- elookout.ino
+++ elookout.ino
@@ -1,15 +1,7 @@
-//************************************************************
-// this is a simple example that uses the painlessMesh library
-//
-// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
-// 2. prints anything it receives to Serial.print
-//
-//
-//************************************************************
 #include "painlessMesh.h"
 
-#define   MESH_PREFIX     "whateverYouLike"
-#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PREFIX     "ElookoutMesh"
+#define   MESH_PASSWORD   "SneakyPassword"
 #define   MESH_PORT       5555
 
 Scheduler userScheduler; // to control your personal task
@@ -18,45 +10,52 @@ painlessMesh  mesh;
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+Task taskSendMessage(TASK_SECOND * 1 ,TASK_FOREVER, &sendMessage);
 
 void sendMessage() {
+  Serial.printf("Sending Message now\r\n");
+  digitalWrite(LED_BUILTIN, HIGH);
   String msg = "Hello from node ";
   msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  mesh.sendBroadcast(msg);
+  taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5));
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  Serial.printf("RECEIVED message from %u msg=%s\r\n", from, msg.c_str());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+    Serial.printf("NEW CONNECTION nodeId = %u\r\n", nodeId);
 }
 
 void changedConnectionCallback() {
-    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+    Serial.printf("CHANGED CONNECTIONS %s\r\n",mesh.subConnectionJson().c_str());
 }
 
 void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+    Serial.printf("ADJUSTED TIME %u. Offset = %d\r\n", mesh.getNodeTime(),offset);
 }
 
 void setup() {
   Serial.begin(115200);
+  Serial.printf("Turning on!\r\n");
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  //mesh.setDebugMsgTypes(ERROR | STARTUP | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
 
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
   mesh.onReceive(&receivedCallback);
   mesh.onNewConnection(&newConnectionCallback);
   mesh.onChangedConnections(&changedConnectionCallback);
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
-  userScheduler.addTask( taskSendMessage );
+  userScheduler.addTask(taskSendMessage);
   taskSendMessage.enable();
 }
 
